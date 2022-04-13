bd774834d3df385a73ee84974906a5dfe8f1d8c7
bukky300 <bukkybrown11@gmail.com>
2022-04-13 07:48:31 +0100

    added some mobile responsiveness

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,8 @@ function Layout(props) {
   return (
     <>
       <div className="w-screen">
-        <div className="flex justify-center">
-          <div className="w-[96%]">
-            <h1 className="  text-3xl font-bold mt-12">Dashboard</h1>
-          </div>
+        <div className="flex justify-start ml-4">
+          <h1 className="  text-3xl font-bold mt-12">Dashboard</h1>
         </div>
         <>{props.children}</>
       </div>
